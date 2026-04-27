79cd5e6b747a32f5871cf3c8200707a25c30e3be
wsk4 <maa.gonzalezva@duocuc.cl>
2026-04-26 22:58:06 -0400

    v1

---
+++ src/main/java/com/sanosysalvos/geolocalizacion/GeolocalizacionApplication.java
@@ -0,0 +1,13 @@
+package com.sanosysalvos.geolocalizacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeolocalizacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeolocalizacionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sanosysalvos/geolocalizacion/GeolocalizacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanosysalvos.geolocalizacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeolocalizacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
